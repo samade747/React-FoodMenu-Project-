6d74b3dbe0b936ad70bda90204e1eef95f15c8dc
samade747 <samad.e747@gmail.com>
2024-02-04 22:14:04 +0500

    1013pm

--- foodmenuapp/src/Food.js
+++ foodmenuapp/src/Food.js
@@ -3,13 +3,14 @@ export default function Food({foodDetail}){
     return (
         <div style={{width:"350px", display: "flex", margin:"20px auto"}}>
             
-                <img width={"37%"}  src={foodDetail.srcImg} alt="foodItems"/>
+                <img width={"45%"}  src={foodDetail.srceImg} alt="foodItems"/>
             
             <div style={{marginLeft: "20px", display: "flex", flexDirection:"column"}}>
             <h4 style={{marginTop: "0px", marginBottom: "0px"}}>{foodDetail.foodName}</h4>
             <p style={{fontSize:"14px"}}><i>{foodDetail.description}</i></p>
             <p style={{marginBottom:"0px"}}>{foodDetail.remaining == "0" ? "SOLD OUT" : foodDetail.remaining}</p>
             </div>
+
         </div>
     )
 }
